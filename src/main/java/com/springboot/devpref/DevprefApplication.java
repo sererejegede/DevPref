4932b93cd03fe64825280274f7792f6f01a3b6d4
Serere Jegede <sererejegede@gmail.com>
2018-02-06 09:37:33 +0100

    Initial commit

---
+++ src/main/java/com/springboot/devpref/DevprefApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.devpref;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevprefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevprefApplication.class, args);
+	}
+}
---
+++ src/main/java/com/springboot/devpref/entity/Developer.java
@@ -0,0 +1,9 @@
+package com.springboot.devpref.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Developer {
+}
